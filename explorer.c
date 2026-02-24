d9a30029dec4a55862f059ee724f770680b60b9c
Thiago <thiago08benjamim@gmail.com>
2026-02-24 01:18:18 -0300

    Adição de arquivo principal sem conteúdo relevante

---
+++ explorer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stddef.h>
+
+int main(){
+    printf("Welcome to ReadySetCode\n");
+    printf("What programming language would you like to install?");
+    
+
+    return 0;
+}
