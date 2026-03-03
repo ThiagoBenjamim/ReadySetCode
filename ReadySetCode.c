5f08bb28fb4222c6fa87e3327a37ace77c7a4215
Thiago <thiago08benjamim@gmail.com>
2026-03-02 21:14:36 -0300

    Adição de controle de resposta do usuário

--- ReadySetCode.c
+++ ReadySetCode.c
@@ -6,7 +6,7 @@ int menuInstall(char *langs[], int numLangs);
 int main(){
     printf("Welcome to ReadySetCode\n\n");
     
-    char *langs[] = {"Python", "C/C++"};
+    char *langs[] = {"Quit program", "Python", "C/C++",};
     int numLangs = sizeof(langs)/sizeof(langs[0]);
 
     printf("%d", menuInstall(langs, numLangs));
@@ -16,13 +16,25 @@ int main(){
 
 int menuInstall(char *langs[], int numLangs){
     int choice;
+    int repeat = 1;
     printf("What programming language would you like to install?\n\n");
 
     for (int i = 0; i < numLangs; i++){
         printf("[%d] %s\n", i + 1, langs[i]);
     }
+    printf("\n");
+
     printf("Select the number of your choice your choice: ");
-    scanf("%d", &choice);
+    while (repeat == 1){
+        scanf("%d", &choice);
+
+        if (choice < 0 || choice >= numLangs){
+            printf("Invalid choice, try again: ");
+        }
+        else{
+            repeat = 0;
+        }
+    }
 
     return choice;
 }
