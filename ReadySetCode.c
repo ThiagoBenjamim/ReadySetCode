b7e09b053823dc4444e8b89652ced4743e6a7ff0
Thiago <thiago08benjamim@gmail.com>
2026-03-02 21:51:30 -0300

    Adição da opção de instalação do Python e verificador de respostas y/n

--- ReadySetCode.c
+++ ReadySetCode.c
@@ -3,15 +3,30 @@
 
 int menuInstall(char *langs[], int numLangs);
 
+int ynCheck();
+
 int main(){
     printf("Welcome to ReadySetCode\n\n");
+
+    int ynChoice;
     
     char *langs[] = {"Quit program", "Python", "C/C++",};
     int numLangs = sizeof(langs)/sizeof(langs[0]);
 
-    printf("%d", menuInstall(langs, numLangs));
+    switch (menuInstall(langs, numLangs)){
+        case 0:
+            return 0;
 
-    return 0;
+        case 1:
+            printf("Would you like to install python for all users? Y/N: ");
+            ynChoice = ynCheck();
+            if (ynChoice == 1){
+                system();
+            }
+            else{
+                system();
+            }
+    }
 }
 
 int menuInstall(char *langs[], int numLangs){
@@ -20,7 +35,7 @@ int menuInstall(char *langs[], int numLangs){
     printf("What programming language would you like to install?\n\n");
 
     for (int i = 0; i < numLangs; i++){
-        printf("[%d] %s\n", i + 1, langs[i]);
+        printf("[%d] %s\n", i, langs[i]);
     }
     printf("\n");
 
@@ -37,4 +52,20 @@ int menuInstall(char *langs[], int numLangs){
     }
 
     return choice;
+}
+
+int ynCheck(){
+    char choice;
+    scanf("%c", &choice);
+    while (1 == 1){
+        if (choice = "Y", "y"){
+            return 1;
+        }
+        else if (choice == "N", "n"){
+            return 0;
+        }
+        else{
+            printf("Invalid choice, try again: ");
+        }
+    }
 }
